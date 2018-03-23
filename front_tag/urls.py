4aeae31a9fd212b47737f59b75affde11d2e5f1e
ivmafo <ivmafo@gmail.com>
2018-03-23 17:43:13 -0500

    1.0.0.1

--- front_tag/urls.py
+++ front_tag/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 
 from . import views
 
+# definicion de espacio de nombre de aplicacion
+app_name = 'front_tag'
+
 urlpatterns = [
     path('', views.index, name='index'),
     # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/specifics/12/      ### tomaria estal ultima definion de name="detail"
+    #path('specifics/<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
