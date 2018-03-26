3a25429ceaba5ee167de8c884ee563a49fae9494
ivmafo <ivmafo@gmail.com>
2018-03-26 17:11:11 -0500

    1.0.0.2

--- front_tag/views.py
+++ front_tag/views.py
@@ -6,30 +6,47 @@ from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
+from django.views import generic
 
 from .models import Question, Choice
 
+class IndexView(generic.ListView):
+    template_name = 'front_tag/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+"""def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'front_tag/index.html', context)
+    return render(request, 'front_tag/index.html', context)"""
 
-def detail(request, question_id):
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'front_tag/detail.html'
+
+"""def detail(request, question_id):
     #return HttpResponse("You're looking at question %s." % question_id)
-    """try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist ....")
-    return render(request, 'front_tag/detail.html',{'question': question})"""
+    #try:
+    #    question = Question.objects.get(pk=question_id)
+    #except Question.DoesNotExist:
+    #    raise Http404("Question does not exist ....")
+    #return render(request, 'front_tag/detail.html',{'question': question})
     question = get_object_or_404(Question , pk=question_id)
-    return render(request,'front_tag/detail.html',{'question':question})
+    return render(request,'front_tag/detail.html',{'question':question})"""
 
-def results(request, question_id):
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'front_tag/results.html'
+
+"""def results(request, question_id):
     #response = "You're looking at the results of question %s."
     #return HttpResponse(response % question_id)
     question = get_object_or_404(Question , pk=question_id )
-    return render(request,'front_tag', {'question':question})
+    #return render(request,'front_tag', {'question':question})
+    return render(request, 'front_tag/results.html',{'question': question})"""
 
 def vote(request, question_id):
     #return HttpResponse("You're voting on question %s." % question_id)
